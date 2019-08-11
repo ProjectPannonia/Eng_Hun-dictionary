e9d6889c9ea9a4121e74a60cf71efa4309831ebd
Adam Letenyei <letenyeidam89@gmail.com>
2019-08-11 20:18:11 +0200

    Added new class - CreatingHashMapFromWords

--- src/AddWordsFromFile/CreatingHashMapFromWords.java
+++ src/AddWordsFromFile/CreatingHashMapFromWords.java
@@ -5,13 +5,14 @@ import java.util.HashMap;
 public class CreatingHashMapFromWords {
     public HashMap createHashMap(String[] array){
         HashMap<String,String> created = new HashMap<>();
-        String eng;
-        String hun;
+        String eng = null;
+        String hun = null;
 
         for (int i = 0; i < array.length; i++){
-
+            if(i % 2 == 0)eng = array[i];
+            else hun = array[i];
+            created.put(eng,hun);
         }
-
         return created;
     }
 }
